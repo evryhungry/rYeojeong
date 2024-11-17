f2b833dbea312418bcd635e156254e0413084ddd
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-11-17 20:34:43 +0900

    create page dart

---
+++ lib/pages/addCommunity.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AddcommunityPage extends StatefulWidget {
+  const AddcommunityPage({super.key});
+  
+  @override
+  State<AddcommunityPage> createState() => _AddcommunityPageState();
+}
+
+class _AddcommunityPageState extends State<AddcommunityPage> {
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
